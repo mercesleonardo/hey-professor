113f2b5c767ac4870bc507227281f4650c3c35a7
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-11 11:06:17 -0300

    HEY-12: it should have at least 10 characters

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -26,4 +26,16 @@ it('should check if ends with question mark ?', function () {
 
 it('should have at least 10 characters', function () {
 
+    // Arrange :: preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act :: agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8) . '?',
+    ]);
+
+    // Assert :: verificar
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
 });
