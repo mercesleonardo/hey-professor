b53f025033e5e691f51e6fca07348b6974b04831
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-12 19:27:21 -0300

    hey-11: adding global function file

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -20,3 +20,19 @@ it('should de able to like a question', function () {
         'user_id'     => $user->id,
     ]);
 });
+
+it('should de able to like more then 1 time', function () {
+    $user = User::factory()->create();
+
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
+
+});
