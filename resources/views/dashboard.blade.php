b157fea4f6cd339db7536ed60dc1b4ade7233897
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-12 01:11:32 -0300

    hey-11: Being able to see the list of questions in the dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,5 +12,16 @@
             <x-btn.primary>Save</x-btn.primary>
             <x-btn.reset>Cancel</x-btn.reset>
         </x-form>
+
+        <hr class="border-gray-700 border-dashed my-4">
+
+        {{-- listagem --}}
+
+        <div class="dark:text-gray-300 uppercase font-bold mb-1">List of Questions</div>
+        <div class="dark:text-gray-400 space-y-4">
+            @foreach ($questions as $item)
+                <x-question :question="$item" />
+            @endforeach
+        </div>
     </x-container>
 </x-app-layout>
