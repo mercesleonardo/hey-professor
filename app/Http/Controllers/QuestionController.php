701919b3a226b5bb56b8aa11f603f212803e27e4
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-15 11:44:38 -0300

    hey-18: make sure that is validating everything when update a question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -51,6 +51,19 @@ class QuestionController extends Controller
     {
         Gate::authorize('update', $question);
 
+        request()->validate([
+            'question' => [
+                'required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if ($value[strlen($value) - 1] !== '?') {
+                        $fail('Are you sure that is a question? It is missing the question mark in the end.');
+                    }
+
+                },
+            ],
+        ]);
+
         $question->question = request()->question;
         $question->save();
 
