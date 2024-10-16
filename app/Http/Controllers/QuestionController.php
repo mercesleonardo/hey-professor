8d6a57b09aa9430f364a4f2696230cf91d78a188
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-16 14:39:56 -0300

    hey-22: implementing the frontend for the archive/restore options

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -13,7 +13,8 @@ class QuestionController extends Controller
     public function index(): View
     {
         return view('question.index', [
-            'questions' => auth()->user()->questions,
+            'questions'         => auth()->user()->questions,
+            'archivedQuestions' => auth()->user()->questions()->onlyTrashed()->get(),
         ]);
     }
 
