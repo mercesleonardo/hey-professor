aaf43a4fa5c243b6ca18bbb125f3aa88b0d1cbbf
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-16 20:50:46 -0300

    hey-26: it should be able to search a question by text

---
+++ tests/Feature/Question/SearchTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, get};
+
+it('should be able to search a question by text', function () {
+
+    $user           = User::factory()->create();
+    $wrongQuestions = Question::factory()->create(['question' => 'Something else?']);
+    $question       = Question::factory()->create(['question' => 'My question is?']);
+
+    actingAs($user);
+
+    $response = get(route('dashboard', ['search' => 'question']));
+
+    $response->assertDontSee('Something else?');
+    $response->assertSee('My question is?');
+
+});
