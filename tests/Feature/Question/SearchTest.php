1acd48a0fade93220fee452df2dcbfce2122aa85
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-17 11:13:52 -0300

    hey-30: conditional where added

--- tests/Feature/Question/SearchTest.php
+++ tests/Feature/Question/SearchTest.php
@@ -7,8 +7,8 @@ use function Pest\Laravel\{actingAs, get};
 it('should be able to search a question by text', function () {
 
     $user           = User::factory()->create();
-    $wrongQuestions = Question::factory()->create(['question' => 'Something else?']);
-    $question       = Question::factory()->create(['question' => 'My question is?']);
+    $wrongQuestions = Question::factory()->create(['question' => 'Something else?', 'draft' => false]);
+    $question       = Question::factory()->create(['question' => 'My question is?', 'draft' => false]);
 
     actingAs($user);
 
